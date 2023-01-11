b8fb8c1f59e0f2c6c95532d08a97ce61eee694fc
phani-ka <39553262+phani-ka@users.noreply.github.com>
2023-01-11 15:46:46 +0530

    Create docker_build_push.sh

---
+++ docker_build_push.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Fail fast
+set -e
+
+# This is the order of arguments
+
+aws_ecr_repository_url_with_tag=$2
+
+# Check that aws is installed
+which aws > /dev/null || { echo 'ERROR: aws-cli is not installed' ; exit 1; }
+
+# Connect into aws
+$(aws ecr get-login --no-include-email) || { echo 'ERROR: aws ecr login failed' ; exit 1; }
+
+# Check that docker is installed and running
+which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
+
+# Some Useful Debug
+echo "Building $aws_ecr_repository_url_with_tag from Dockerfile"
+
+# Build image
+docker build -t $aws_ecr_repository_url_with_tag Dockerfile
+
+# Push image
+docker push $aws_ecr_repository_url_with_tag
