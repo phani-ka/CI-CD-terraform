b4696d02f436d63a3ac0820aa14b55dcfa928978
phani-ka <39553262+phani-ka@users.noreply.github.com>
2023-01-12 16:26:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,5 +45,16 @@ resource "null_resource" "push" {
   }
 
 }
-
-
+resource "aws_ecs_cluster" "ecs_cluster" {
+    name  = "phani-demo-cluster"
+}
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "worker"
+  container_definitions = data.template_file.task_definition_template.rendered
+}
+resource "aws_ecs_service" "worker" {
+  name            = "worker"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 2
+}
