73bf5607b3b923e4430ec9362c8019d6472c0d21
phani-ka <39553262+phani-ka@users.noreply.github.com>
2023-01-23 07:21:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -163,8 +163,8 @@ resource "aws_alb" "application_load_balancer" {
 # Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = 8080 # Allowing traffic in from port 80
-    to_port     = 8080
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
   }
@@ -178,7 +178,7 @@ resource "aws_security_group" "load_balancer_security_group" {
 }
 resource "aws_lb_target_group" "target_group" {
   name        = "target-group"
-  port        = 8080
+  port        = 80
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
@@ -190,7 +190,7 @@ resource "aws_lb_target_group" "target_group" {
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
-  port              = "8080"
+  port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
